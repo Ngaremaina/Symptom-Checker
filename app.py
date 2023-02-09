31d76dc2f1a941b8ce9040ed3926503e376d8c6c
Ngare <45742522+Ngaremaina@users.noreply.github.com>
2023-02-09 23:16:31 +0300

    Symptom Checker

--- app.py
+++ app.py
@@ -15,7 +15,6 @@ def getprecautionDict():
     global precautionDictionary
     with open('data/Precautions.csv') as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
-        line_count = 0
         for row in csv_reader:
             _prec={row[0]:[row[1],row[2],row[3],row[4]]}
             precautionDictionary.update(_prec)
@@ -37,7 +36,6 @@ def tree_to_code(tree, feature_names):
 
     symptoms_present = []
     def recurse(node, depth):
-        indent = "  " * depth
         if tree_.feature[node] != _tree.TREE_UNDEFINED:
             name = feature_name[node]
             threshold = tree_.threshold[node]
